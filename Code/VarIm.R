5e1b7d0e15c4efdb7e89d95d1d9b6edfbede1046
Kanny <493768967@qq.com>
2020-09-01 21:00:43 +0800

    Tidy the codes up
    Tidy the codes up

--- Code/VarIm.R
+++ Code/VarIm.R
@@ -1,106 +1,91 @@
-###########------------1
+# ------------- Packages ---------------
+library(MASS)
+library(mpath)
+
+# ------------- Part I -----------------
+# ----- load data
 load("FIPScumdead.rda")
 ss <- var.57[[1]]
+ss.possion1 <- ss[, which(colnames(ss) %in% 
+                            c("FIPS", "cum","dead_15","Population", "Segregation index-non-White/White",
+                              "% Long Commute - Drives Alone", "% Rural","% Drive Alone to Work", 
+                              "% Hispanic", "% With Access to Exercise Opportunities", 
+                              "% Female", "Average Daily PM2.5", "% Insufficient Sleep",
+                              "% 65 and over","Primary Care Physicians Ratio"))]
 
-ss.possion1 <- ss[, which(colnames(ss)%in%c("FIPS", "cum","dead_15","Population", "Segregation index-non-White/White",
-                                            "% Long Commute - Drives Alone", "% Rural","% Drive Alone to Work", "% Hispanic", 
-                                            "% With Access to Exercise Opportunities", 
-                                            "% Female", "Average Daily PM2.5", "% Insufficient Sleep",
-                                            "% 65 and over","Primary Care Physicians Ratio"))]
-
-set.seed(112) 
-library(MASS)
-library(mpath)
-train1 <- sample(1:dim(ss.possion1)[1], dim(ss.possion1)[1]/10*7)
-total.mod1 <-  glm.nb(dead_15 ~ ., data = ss.possion1[train1,-which(colnames(ss.possion1)%in%c("FIPS", "cum"))])
-
+# set.seed(112) 
+# ----- fit model
+train1 <- sample(1:dim(ss.possion1)[1], dim(ss.possion1)[1] / 10 * 7)
+total.mod1 <-  glm.nb(dead_15 ~ ., data = ss.possion1[train1, -which(colnames(ss.possion1) %in% c("FIPS", "cum"))])
 summary(total.mod1)
 res.11 <- summary(total.mod1)$coefficient
 
-write.csv(res.11[res.11[, "Pr(>|z|)"]<0.05,], file = "class1.csv" ,fileEncoding = "GBK")
+write.csv(res.11[res.11[, "Pr(>|z|)"] < 0.05, ], file = "class1.csv" , fileEncoding = "GBK")
 
 Pred1 <- predict(total.mod1, newdata = ss.possion1[-train1,-which(colnames(ss.possion1)%in%c("FIPS", "cum"))], type = "response")
 
-allre <- ss.possion1[-train1, which(colnames(ss.possion1)%in%c("FIPS", "cum", "dead_15"))]
-allre$"Pred1" <-Pred1 
+allre <- ss.possion1[-train1, which(colnames(ss.possion1) %in% c("FIPS", "cum", "dead_15"))]
+allre$"Pred1" <- Pred1 
 
-quantile(allre[,'dead_15']/allre[,'cum'])
+quantile(allre[,'dead_15'] / allre[,'cum'])
 
-lis.1 <- list(allre, NB=total.mod1)
-save(lis.1, file = "class1.rda") #
-
-
+lis.1 <- list(allre, NB = total.mod1)
+save(lis.1, file = "class1.rda") 
 
 
-
-###########------------2
-
+# ------------- Part II -----------------
 load("FIPScumdead.rda")
 ss <- var.57[[2]]
-ss.possion1 <- ss[, which(colnames(ss)%in%c("FIPS", "cum","dead_15","Population", "Segregation index-non-White/White",
-                                            "% Long Commute - Drives Alone", "% Rural","% Drive Alone to Work", "% Hispanic", 
-                                            "% With Access to Exercise Opportunities", 
-                                            "% Female", "Average Daily PM2.5", "% Insufficient Sleep",
-                                            "% 65 and over","Primary Care Physicians Ratio"))]
+ss.possion1 <- ss[, which(colnames(ss) %in% 
+                            c("FIPS", "cum","dead_15","Population", "Segregation index-non-White/White",
+                              "% Long Commute - Drives Alone", "% Rural","% Drive Alone to Work", 
+                              "% Hispanic", "% With Access to Exercise Opportunities", 
+                              "% Female", "Average Daily PM2.5", "% Insufficient Sleep",
+                              "% 65 and over","Primary Care Physicians Ratio"))]
 
-
-set.seed(2232) #0.02874536
-library(MASS)
-library(mpath)
-train1 <- sample(1:dim(ss.possion1)[1], dim(ss.possion1)[1]/10*7)
-total.mod1 <-  glm.nb(dead_15 ~ ., data = ss.possion1[train1,-which(colnames(ss.possion1)%in%c("FIPS", "cum"))])
-#total.mod1 <- glmregNB(dead_15 ~ ., alpha=1, data = ss.possion1[train1,-which(colnames(ss.possion1)%in%c("FIPS", "cum"))], lambda=0.21)
-#total.mod1 <- glm(dead_15 ~ ., data = ss.possion1[train1,-which(colnames(ss.possion1)%in%c("FIPS", "cum"))], family = poisson)
-
+# set.seed(2232)
+# ----- fit model
+train1 <- sample(1:dim(ss.possion1)[1], dim(ss.possion1)[1] / 10 * 7)
+total.mod1 <-  glm.nb(dead_15 ~ ., data = ss.possion1[train1, -which(colnames(ss.possion1) %in% c("FIPS", "cum"))])
 summary(total.mod1)
 res.11 <- summary(total.mod1)$coefficient
 
-write.csv(res.11[res.11[, "Pr(>|z|)"]<0.05,], file = "class2.csv" ,fileEncoding = "GBK")
+write.csv(res.11[res.11[, "Pr(>|z|)"] < 0.05, ], file = "class2.csv", fileEncoding = "GBK")
 
+Pred1 <- predict(total.mod1, newdata = ss.possion1[-train1, -which(colnames(ss.possion1) %in% c("FIPS", "cum"))], type = "response")
 
+allre <- ss.possion1[-train1, which(colnames(ss.possion1) %in% c("FIPS", "cum", "dead_15"))]
+allre$"Pred1" <- Pred1 
 
-Pred1 <- predict(total.mod1, newdata = ss.possion1[-train1,-which(colnames(ss.possion1)%in%c("FIPS", "cum"))], type = "response")
+quantile(allre[, 'dead_15'] / allre[, 'cum'])
 
-allre <- ss.possion1[-train1, which(colnames(ss.possion1)%in%c("FIPS", "cum", "dead_15"))]
-allre$"Pred1" <-Pred1 
-
-
-quantile(allre[,'dead_15']/allre[,'cum'])
-
-lis.1 <- list(allre, NB=total.mod1)
+lis.1 <- list(allre, NB = total.mod1)
 save(lis.1, file = "class2.rda") 
 
 
-###########------------3
-
-
-load("FIPScumdead.rda")
+# ------------- Part III -----------------
+# load("FIPScumdead.rda")
 ss <- var.57[[3]]
 
-ss.possion1 <- ss[, which(colnames(ss)%in%c("FIPS", "cum","dead_15","Population", "Segregation index-non-White/White",
-                                            "% Long Commute - Drives Alone", "% Rural","% Drive Alone to Work", "% Hispanic", 
-                                            "% With Access to Exercise Opportunities", 
-                                            "% Female", "Average Daily PM2.5", "% Insufficient Sleep",
-                                            "% 65 and over","Primary Care Physicians Ratio"))]
-set.seed(334)
-library(MASS)
-library(mpath)
-train1 <- sample(1:dim(ss.possion1)[1], dim(ss.possion1)[1]/10*7)
-total.mod1 <-  glm.nb(dead_15 ~ ., data = ss.possion1[train1,-which(colnames(ss.possion1)%in%c("FIPS", "cum"))])
-
+ss.possion1 <- ss[, which(colnames(ss) %in%
+                            c("FIPS", "cum","dead_15","Population", "Segregation index-non-White/White",
+                              "% Long Commute - Drives Alone", "% Rural","% Drive Alone to Work", 
+                              "% Hispanic", "% With Access to Exercise Opportunities", 
+                              "% Female", "Average Daily PM2.5", "% Insufficient Sleep",
+                              "% 65 and over","Primary Care Physicians Ratio"))]
+# set.seed(334)
+# ----- fit model
+train1 <- sample(1:dim(ss.possion1)[1], dim(ss.possion1)[1] / 10 * 7)
+total.mod1 <-  glm.nb(dead_15 ~ ., data = ss.possion1[train1, -which(colnames(ss.possion1) %in% c("FIPS", "cum"))])
 summary(total.mod1)
 res.11 <- summary(total.mod1)$coefficients
-res.11
 
-write.csv(res.11[res.11[, "Pr(>|z|)"]<0.05,], file = "class3.csv" ,fileEncoding = "GBK")
+write.csv(res.11[res.11[, "Pr(>|z|)"] < 0.05, ], file = "class3.csv", fileEncoding = "GBK")
 
-Pred1 <- predict(total.mod1, newdata = ss.possion1[-train1,-which(colnames(ss.possion1)%in%c("FIPS", "cum"))], type = "response")
+Pred1 <- predict(total.mod1, newdata = ss.possion1[-train1, -which(colnames(ss.possion1) %in% c("FIPS", "cum"))], type = "response")
 
-allre <- ss.possion1[-train1, which(colnames(ss.possion1)%in%c("FIPS", "cum", "dead_15"))]
+allre <- ss.possion1[-train1, which(colnames(ss.possion1) %in% c("FIPS", "cum", "dead_15"))]
 allre$"Pred1" <-Pred1 
 
-lis.1 <- list(allre,NB=total.mod1)
+lis.1 <- list(allre,NB = total.mod1)
 save(lis.1, file = "class3.rda") 
-
-
-
--- Code/clustering.R
+++ Code/clustering.R
@@ -1,32 +1,37 @@
-norm_z<-function(x){
-  nx=(x-mean(x))/norm(x-mean(x),type ="2" )
-}
-
+# ------------------ Packages -------------------
 library(clusterCrit)
 library(ggplot2)
 library(data.table)
 library(cluster)
 
+# ------------------ Functions ------------------
+norm_z <- function(x) {
+  nx <- (x - mean(x)) / norm(x - mean(x), type = "2")
+}
+
+# ------------------ Main -----------------------
+# ----- load data
 load("cum_confirm.rda")
-ter.data=cum_confirm[-which(cum_confirm[,46]==0),]
+
+# ----- data preprocessing
+ter.data <- cum_confirm[-which(cum_confirm[, 46] == 0), ]
 cum_confirm_n <- apply(ter.data, 2, norm_z)
 colnames(cum_confirm_n) <- seq(1,46, length.out = 46)
 rownames(cum_confirm_n) <- rownames(ter.data) 
 
-# clustering
+# ----- clustering
 clusterings <- lapply(c(2:7), function(x)
   pam(cum_confirm_n, x))
+
 KD_values <- sapply(seq_along(clusterings), function(x) 
   intCriteria(cum_confirm_n, as.integer(clusterings[[x]]$clustering),
               c("Ksq_DetW")
   ))
-# plot the results
-ggplot(data.table(Clusters = 2:7, KD_index = unlist(KD_values)),
+
+# ----- plot the results
+ggplot(data.table(Clusters = 2:7, KD_index = unlist(KD_values)), 
        aes(Clusters, KD_index)) +
   geom_line(size = 1) +
   geom_point(size = 3) +
   theme_bw()
-
-
-
 
